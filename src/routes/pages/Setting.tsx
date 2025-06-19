10b9b7d781c201b14ff26caf32bc5e5291cfff57
HYK <yundori97@gmail.com>
2025-06-19 20:40:53 +0900

    feat: 다크모드 전환 기능

--- src/routes/pages/Setting.tsx
+++ src/routes/pages/Setting.tsx
@@ -1,4 +1,8 @@
+import { useTheme } from "@/components/ThemeProvider.tsx";
+
 export default function SettingPage() {
+  const { setTheme } = useTheme();
+
   return (
     <>
       <div
@@ -7,6 +11,33 @@ export default function SettingPage() {
         }
       >
         <p className={"text-xl font-bold"}>설정</p>
+      </div>
+      <div className={"px-5 pt-10 pb-15"}>
+        <div>
+          <h1 className={"pb-3 text-2xl font-bold"}>테마</h1>
+          <div className={"grid grid-cols-3 gap-3"}>
+            <button
+              onClick={() => setTheme("system")}
+              className={"rounded border py-2"}
+            >
+              시스템
+            </button>
+            <button
+              onClick={() => setTheme("light")}
+              className={"rounded border bg-white py-2 text-black"}
+            >
+              라이트
+            </button>
+            <button
+              onClick={() => setTheme("dark")}
+              className={
+                "bg-background dark text-foreground rounded border py-2"
+              }
+            >
+              다크
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
